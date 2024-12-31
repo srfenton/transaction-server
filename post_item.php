fd7597c5e1d61be0f51a552ce6fa92c75dd05bc6
sf <sreedfenton@gmail.com>
2024-12-30 16:44:07 -0800

    the add transaction button is working really nicely now and the form is validating nicely too.

--- post_item.php
+++ post_item.php
@@ -7,23 +7,31 @@ $password = $loginData["password"];
 $dbname = $loginData["database"];
 
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-$mysqli = new mysqli($dbname, $username, $password, $dbname);
+$mysqli = new mysqli($servername, $username, $password, $dbname);
 
 $stmt = $mysqli->prepare("INSERT INTO transactions (item, cost, category, payment_method, date, notes) VALUES (?, ?, ?, ?, ?, ?)");
 $stmt->bind_param('sdssss', $item, $cost, $category, $payment_method, $date, $notes);
+if (!$stmt) {
+    die("Prepare failed: " . $mysqli->error);
+}
 
-$item = '';
-$cost = '';
-$category = '';
-$payment_method = '';
-$date = '';
-$notes = '';
+
+$item = htmlspecialchars($_POST["item"]);
+$cost = filter_var($_POST["cost"], FILTER_VALIDATE_FLOAT);
+$category = htmlspecialchars($_POST["category"]);
+$payment_method = htmlspecialchars($_POST["payment_method"]);
+$date = htmlspecialchars($_POST["date"]);
+$notes = htmlspecialchars($_POST["notes"]);
+
+if ($cost === false) {
+    die("Invalid cost value.");
+}
 
 $stmt->execute();
 
 printf("%d row inserted.\n", $stmt->affected_rows);
 
 
-echo 'hello world'
+header('Location: '.'/');
 
 ?>
